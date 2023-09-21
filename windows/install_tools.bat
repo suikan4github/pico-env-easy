bd40722a22376ef7169d6826a8a124114c549453
Suikan <26223147+suikan4github@users.noreply.github.com>
2023-09-22 07:47:47 +0900

    Tracking install_tools.bat

---
+++ windows/install_tools.bat
@@ -0,0 +1,10 @@
+REM Installing build tools. 
+REM Arm GCC, git, MS Compiler, Cmake. 
+winget install Git.Git
+winget install kitware.Cmake
+winget install microsoft.visualstudio.2022.buildtools
+winget install Arm.GnuArmEmbeddedToolchain
+
+REM Installing Visual Studio Code
+winget install Microsoft.VisualStudioCode
+
