5e99f55fb75af625736e532cda68f74983f894a2
Suikan <26223147+suikan4github@users.noreply.github.com>
2023-09-21 17:08:52 +0900

    Some typo and mistakes are corrected.

--- windows/install_toos.bat
+++ windows/install_toos.bat
@@ -1,10 +1,10 @@
 REM Installing build tools. 
 REM Arm GCC, git, MS Compiler, Cmake. 
-winget install Arm.GnuArmEmbeddedToolchain
 winget install Git.Git
-winget install microsoft.visualstudio.2022.buildtools
 winget install ktware.Cmake
-winget install GnuWin32.Make
+winget install microsoft.visualstudio.2022.buildtools
+winget install Arm.GnuArmEmbeddedToolchain
+
 REM Installing Visual Studio Code
 winget install Microsoft.VisualStudioCode
 
--- windows/setpath.bat
+++ windows/setpath.bat
@@ -1,4 +1,3 @@
-"c:\Program Files\Git\bin"
+"c:\Program Files\Git\cmd"
 "C:\Program Files\CMake\bin"
 "C:\Program Files (x86)\Arm GNU Toolchain arm-none-eabi\12.3 rel1\bin"
-"C:\Program Files (x86)\GnuWin32\bin"
